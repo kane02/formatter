653915905d22e1229734c5e875970e42addfec70
Aram Petrosyan <kane_02@mail.ru>
2016-04-23 19:45:18 +0700

    added Main class

---
+++ src/main/java/it/sevenbits/course/bootstrap/Main.java
@@ -0,0 +1,22 @@
+package it.sevenbits.course.bootstrap;
+
+/**
+ * Enter point of project
+ */
+public class Main {
+
+    /**
+     * default constructor
+     */
+    public Main() {
+
+    }
+
+    /**
+     * Enter point of project
+     * @param args of incoming arguments
+     */
+    public static void main(final String[] args) {
+        
+    }
+}
---
+++ src/main/java/it/sevenbits/course/bootstrap/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains class Main
+ * Enter point of project
+ */
+package it.sevenbits.course.bootstrap;
