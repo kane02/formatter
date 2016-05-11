1cc2298372b2ef4ac9639474b26369833d1e6d42
Aram Petrosyan <kane_02@mail.ru>
2016-05-11 20:39:49 +0700

    edit FileReader

--- src/main/java/it/sevenbits/course/reader/FileReader.java
+++ src/main/java/it/sevenbits/course/reader/FileReader.java
@@ -1,36 +1,70 @@
 package it.sevenbits.course.reader;
 
+import com.sun.xml.internal.ws.Closeable;
 import it.sevenbits.course.reader.IReader;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 /**
  * FileReader  class
  * Providing an read input stream from the file
  */
-public class FileReader implements IReader {
+public class FileReader implements IReader , Closeable {
 
     private FileInputStream fileInputStream;
 
-    /*
-
+    /**
+     * Constructs an object for reading stream from file
+     * It contains FileInputStream for provide Input Stream
+     * @param filename is a name of the open
+     * @throws ReaderException
      */
-    public FileReader(final String name) {
-
+    public FileReader(final String filename) throws ReaderException {
+        try {
+            this.fileInputStream = new FileInputStream(filename);
+        } catch (FileNotFoundException exc) {
+            throw new ReaderException("File not found", exc);
+        }
     }
-    /*
 
+    /**
+     * Method of reading char from the stream
+     * Read next symbol if it exist
+     * @return next char symbol
+     * @throws ReaderException
      */
-    public char readChar() {
-        return 0;
+    public char readChar() throws ReaderException {
+        try {
+            return (char) fileInputStream.read();
+        } catch (IOException e) {
+            throw new ReaderException("Can not read file", e);
+        }
     }
 
-    public boolean isEnd() {
-        return false;
+    /**
+     * Method of checking of end of file
+     * Checking end of file
+     * @return true if it end of file
+     */
+    public boolean isEnd() throws ReaderException {
+        try {
+            return fileInputStream.available() == 0;
+        } catch (IOException e) {
+            throw new ReaderException("Error with work with file", e);
+        }
     }
 
-    public void close() throws IOException {
-
+    /**
+     * Method of closing of stream
+     * @throws IOException
+     */
+    public void close() {
+        try {
+            fileInputStream.close();
+        } catch (IOException exc) {
+            exc.fillInStackTrace();
+        }
     }
 }
--- src/main/java/it/sevenbits/course/reader/IReader.java
+++ src/main/java/it/sevenbits/course/reader/IReader.java
@@ -5,20 +5,22 @@ import java.io.Closeable;
 /**
  * IReader interface
  * This is abstraction of read input stream from some source
+ * It extends Closeable interface for getting method close()
  */
-    interface IReader extends Closeable {
+    interface IReader {
 
     /**
      * reading char symbol if it exists
      * @return next symbol, one char
+     * @throws ReaderException
      */
-    char readChar();
+    char readChar() throws ReaderException;
 
 
     /**
      * check end of stream
      * @return boolean true if it end of stream
      */
-    boolean isEnd();
+    boolean isEnd() throws ReaderException;
 
 }
