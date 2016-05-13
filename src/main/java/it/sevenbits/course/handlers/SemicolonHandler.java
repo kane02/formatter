7e7a52ba0c0f2eb2c32e134a8d5ccb6e57023c95
Aram Petrosyan <kane_02@mail.ru>
2016-05-13 23:52:49 +0700

    add semicolon handler

---
+++ src/main/java/it/sevenbits/course/handlers/SemicolonHandler.java
@@ -0,0 +1,26 @@
+package it.sevenbits.course.handlers;
+
+import it.sevenbits.course.writer.WriterException;
+
+/**
+ * Provide handler of formatting code
+ * Work with semicolon symbol
+ */
+public class SemicolonHandler implements  IHandler {
+    /**
+     * Method of formatting of the code
+     * @param streamContextManager is interface for classes for manage work during format
+     */
+    public void format(final IStreamContextManager streamContextManager) {
+        if (streamContextManager.getReadChar() != ';')
+            return ;
+        try {
+            streamContextManager.writeString(";");
+            streamContextManager.writeString("\n");
+            for (int i = 0; i < streamContextManager.getIndentLevel() * streamContextManager.getIndentSize(); i++)
+                streamContextManager.writeChar(' ');
+        } catch (WriterException e) {
+            e.fillInStackTrace();
+        }
+    }
+}
